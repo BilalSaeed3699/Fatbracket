701d330796e7570e12b7d2c2068de58357db4339
HASSAN SAEED <hassanchau93@yahoo.co.uk>
2023-01-05 12:25:07 +0500

    Dashboard assets files

---
+++ Fatbracket/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fatbracket.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            try
+            {
+
+                return View();
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+            return View();
+        }
+    }
+}
